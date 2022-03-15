25bb16a3b158bfc3d7d0d37276e181e0da15cacc
Vgeorgee <cracken1111@gmail.com>
2022-03-15 18:54:31 +0100

    NEP-20/fix: generateBuffer is not async...

--- utils/utils.js
+++ utils/utils.js
@@ -5,5 +5,10 @@ const HASH_ALGORITHM = 'sha256'
 module.exports = {
     hash(plaintext){
         return crypto.createHash(HASH_ALGORITHM).update(plaintext).digest('hex');
+    },
+    
+    generateToken() {
+        const buffer = crypto.randomBytes(48);
+        return buffer.toString('hex')
     }
 }
