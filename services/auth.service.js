7098d3933a701ee8996a9dc0934e4f3b3fea6252
Vgeorgee <cracken1111@gmail.com>
2022-03-15 18:53:21 +0100

    NEP-20/feat: make auth service work

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,17 +1,27 @@
 "use strict";
 
 const requests = require('../commons/requests.json').auth
-const { hash } = require('./utils/utils')
+const responses = require('../commons/responses.json').auth
+const USER_ENDPOINTS = require('./user.service').ENDPOINTS
+const { generateToken } = require('../utils/utils');
+const { hash } = require('../utils/utils')
 
 
 /**
  * @typedef {import('moleculer').Context} Context Moleculer's Context
  */
 
+const authTokens = {}
+
 module.exports = {
 	name: "auth",
     
 	settings: {},
+
+    ENDPOINTS: {
+        REGISTER: "auth.register",
+        LOGIN: "auth.login"
+    },
 
 	dependencies: [],
 
@@ -21,30 +31,71 @@ module.exports = {
 			/** @param {Context} ctx  */
 			async handler(ctx) {
 				const userData = ctx.params
-				const exists = ctx.broker.call('user.exists', userData.email)
+				const exists = await ctx.broker.call(USER_ENDPOINTS.EXISTS, {email: userData.email })
 				if(exists){
 					return false
 				}
+                const userCreationRequest = {...userData}
 				const hashedPassword = hash(userData.password)
-                userData.password = hashedPassword
-				await ctx.broker.call('user.create', userData)
+                userCreationRequest.password = hashedPassword
+				await ctx.broker.call(USER_ENDPOINTS.CREATE, userCreationRequest)
 				return true
 			}
 		},
 
 		login: {
 			params: requests.login,
-			/** @param {Context} ctx  */
+			/** 
+             * @param {Context} ctx 
+             */
 			async handler(ctx) {
-                ctx.params.password = hash(ctx.params.password)
-                return ctx.params
+                const user = await ctx.broker.call(USER_ENDPOINTS.GET, {email: ctx.params.email})
+                const passwordHash = hash(ctx.params.password)
+
+                if(!user || passwordHash !== user.password) {
+                    return responses.login.fail
+                }
+
+                if(user.device){
+                    const deviceToken = await this.createDeviceToken(user.device)
+                    return this.furtherAuthenticationNeeded(deviceToken)
+                }
+
+                const token = await this.createAuthToken(user)
+                return this.successfulAuthentication(token)
 			}
 		},
 	},
 
+    deviceTokens: {},
+
 	events: {},
 
-	methods: {},
+	methods: {
+        async createDeviceToken(device) {
+            const token = await generateToken()
+            this.deviceTokens[device] = token
+            return token
+        },
+
+        async createAuthToken(user) {
+            const token = await generateToken()
+            authTokens[token] = user
+            return token
+        },
+
+        furtherAuthenticationNeeded(deviceToken) {
+            const response = responses.login['2fa']
+            response.loginIdentifier = deviceToken
+            return response
+        },
+
+        successfulAuthentication(token) {
+            const response = responses.login.success
+            response.token = token
+            return response
+        }
+    },
 
 	created() {},
 
