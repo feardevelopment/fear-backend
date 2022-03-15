d88b00b063bab9ec9b2c28255724f8c2c48a7e1e
Vgeorgee <cracken1111@gmail.com>
2022-03-15 18:55:01 +0100

    NEP-20/test: add tests for user service

---
+++ test/unit/services/user.spec.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const { ServiceBroker, Context } = require("moleculer");
+const { ValidationError } = require("moleculer").Errors;
+const TestService = require("../../../services/user.service");
+
+const ENDPOINTS = TestService.ENDPOINTS
+
+describe("Test actions", () => {
+	const broker = new ServiceBroker({ logger: false })
+	const service = broker.createService(TestService)
+
+    console.log(service.adapter);
+	jest.spyOn(service.adapter, "findOne");
+	jest.spyOn(service.adapter, "insert");
+    
+	beforeAll(() => broker.start());
+	afterAll(() => broker.stop());
+    describe('exists', () => {
+        it('should return false when asked for a non-existing user', async () => {
+			// Given
+            const expected = false
+            service.adapter.findOne.mockImplementation(async () => null)
+            const email = "non@existing.user"
+            // When
+            const actual = await broker.call(ENDPOINTS.EXISTS, {email: email})
+            // Then
+            expect(actual).toEqual(expected)
+        })
+
+        it('should return true when asked for an existing user', async () => {
+			// Given
+            const expected = true
+            service.adapter.findOne.mockImplementation(async () => true)
+            const email = "non@existing.user"
+            // When
+            const actual = await broker.call(ENDPOINTS.EXISTS, {email: email})
+            // Then
+            expect(actual).toEqual(expected)
+        })
+    })
+
+    describe('create', () => {
+        it('should call insert function', async () => {
+			// Given
+            service.adapter.insert.mockImplementation(async () => null)
+            const email = "non@existing.user"
+            // When
+            await broker.call(ENDPOINTS.CREATE, {
+                email: email,
+                password: 'password',
+                name: 'name'
+            })
+
+            // Then
+            expect(service.adapter.insert).toHaveBeenCalled()
+        })
+    })
+})
+
