c90d8b904555c35291064d0ff947e7b4eb7e0a45
Vgeorgee <cracken1111@gmail.com>
2022-04-11 20:30:15 +0200

    NEP-20/test: export flow test to integration tests

--- test/unit/services/auth.spec.js
+++ test/unit/services/auth.spec.js
@@ -9,7 +9,6 @@ const responses = require('../../../services/common/responses.json')
 const { hash } = require('../../../utils/utils')
 
 const ENDPOINTS = TestService.ENDPOINTS
-const USER_ENDPOINTS = UserService.ENDPOINTS
 
 describe('Test actions', () => {
 
@@ -154,56 +153,10 @@ describe('Test actions', () => {
 
             // Then
             expect(actual.code).toEqual(expected.code)
-            expect(actual.loginIdentifier).not.toBeFalsy()
-            expect(actual.loginIdentifier.length).toBeGreaterThan(10)
+            expect(actual.flowID).not.toBeFalsy()
+            expect(actual.flowID.length).toBeGreaterThan(10)
             await broker.stop()
         })
     })
-
-
-    describe.skip('Device flows', () => {
-
-        it('should register a new device successfully', async () => {
-            const broker = new ServiceBroker({ logger: false })
-            const service = broker.createService(TestService)
-            const userService = broker.createService(UserService)
-            await broker.start()
-            const registerData = {
-                email: 'nond@fg.user',
-                password: 'pa532sssword',
-                name: 'us432ernadme'
-            }
-
-            const regresp = await broker.call(ENDPOINTS.REGISTER, registerData)
-
-            const loginRequest = await broker.call(ENDPOINTS.LOGIN, registerData)
-
-            const authToken = loginRequest.token
-            // start device registration
-            const deviceRegistrationData = {
-                email: registerData.email,
-                deviceID: '111111-222222'
-            }
-            const deviceActivationRequest = await broker.call(ENDPOINTS.START_DEVICE_ACTIVATION, deviceRegistrationData)
-
-            const token = '000-000'
-            const verifyDeviceActivationRequestData = {
-                identifier: deviceActivationRequest.identifier,
-                token
-            }
-            const verifyDeviceActivationRequest = await broker.call(ENDPOINTS.VERIFY_DEVICE_ACTIVATION, verifyDeviceActivationRequestData)
-
-            expect(verifyDeviceActivationRequest).toBeTruthy()
-
-            const loginRequestWithDevice = await broker.call(ENDPOINTS.LOGIN, registerData)
-            
-            expect(loginRequestWithDevice.code).toEqual(202)
-            expect(loginRequestWithDevice).toHaveProperty('loginIdentifier')
-            await broker.stop()
-            
-        })
-    })
-
-    
 })
 
