33539fcf58ec959eff9b731a137b561209f376d0
Vgeorgee <cracken1111@gmail.com>
2022-04-15 20:27:26 +0200

    NEP-45/refactor: change imports for changed files

--- test/unit/services/auth.spec.js
+++ test/unit/services/auth.spec.js
@@ -5,7 +5,7 @@ const { ValidationError } = require('moleculer').Errors
 const TestService = require('../../../services/auth.service')
 const UserService = require('../../../services/user.service')
 
-const responses = require('../../../services/common/responses.json')
+const responses = require('../../../services/common/response')
 const { hash } = require('../../../utils/utils')
 
 const ENDPOINTS = TestService.ENDPOINTS
